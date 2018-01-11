9cf0f267a9482ddd47e2a70d2afee135a6b3b697
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2018-01-11 13:08:34 +0100

    base proyect structure init

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,22 @@
  * Created by iibarguren on 3/13/17.
  */
 
-var gulp = require('gulp'),
+var babel = require('gulp-babel'),
     concat = require('gulp-concat'),
-    cssnext = require('postcss-cssnext'),
     cssnano = require('gulp-cssnano'),
-    livereload = require('gulp-livereload'),
-    modernizr = require('gulp-modernizr'),
-    merge = require('merge-stream'),
-    plumber = require('gulp-plumber'),
-    postcss = require('gulp-postcss'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    scsslint = require('gulp-scss-lint'),
     del = require('del'),
-    uglify = require('gulp-uglify'),
-    notify = require("gulp-notify"),
-    minify = require('gulp-minify'),
-    bower = require('gulp-bower'),
-    babel = require('gulp-babel');
+    gulp = require('gulp'),
+    merge = require('merge-stream'),
+    minify = require('gulp-minify')
+    ;
 
 var config = {
     sassPath: './app/Resources/assets/scss',
-    bowerDir: './app/Resources/assets/js'
+    bowerDir: './app/Resources/assets/js',
+    yarnDir:  './node_modules'
+
 };
+
 
 var paths = {
     npm: './node_modules',
@@ -37,183 +30,73 @@ var paths = {
     buildSvg: './web/svg'
 };
 
-otherFonts = [
-    config.bowerDir + '/font-awesome/fonts/**.*',
-    config.bowerDir + '/bootstrap/fonts/**.*'
+myFonts = [
+    config.yarnDir + '/font-awesome/fonts/**.*',
+    config.yarnDir + '/bootstrap/fonts/**.*'
 ];
 
-freeIMG = [
-    './app/Resources/assets/js/bootstrap-colorpicker/dist/img/**/*',
-    './app/Resources/assets/img/**/*'
-];
-freeJS = [
-    './app/Resources/assets/js/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js',
-    './app/Resources/assets/js/vertical-timeline/vertical-timeline.js',
-    './app/Resources/assets/js/calendar-template.js',
-    './app/Resources/assets/js/calendar-admin.js',
-    './app/Resources/assets/js/calendar-edit-admin.js',
-    './app/Resources/assets/js/calendar-user.js',
-    './app/Resources/assets/js/calendar-compare.js',
-    './app/Resources/assets/js/libs/select2.min.js',
-    './app/Resources/assets/js/libs/select2-locale/es.js',
-    './app/Resources/assets/js/libs/select2-locale/eu.js',
-    './app/Resources/assets/js/calendar-share.js'
+myJS = [
+    config.yarnDir + "/bootstrap/dist/js/bootstrap.min.js",
+    config.yarnDir + "/jquery/dist/jquery.min.js"
 ];
 
-freeCSS = [
-    './app/Resources/assets/js/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css'
+myCSS = [
+    config.yarnDir + '/bootstrap/dist/css/bootstrap.min.css',
+    config.yarnDir + '/bootstrap/dist/css/bootstrap-theme.min.css',
+    config.yarnDir + '/font-awesome/css/font-awesome.min.css'
 ];
 
-otherJS = [
-    './app/Resources/assets/js/libs/respond.min.js',
-    './app/Resources/assets/js/jquery/dist/jquery.min.js',
-    './app/Resources/assets/js/libs/jquery-ui.js',
-    './app/Resources/assets/js/bootstrap/dist/js/bootstrap.min.js',
-    './app/Resources/assets/js/bootstrap-year-calendar/js/bootstrap-year-calendar.min.js',
-    './app/Resources/assets/js/bootstrap-year-calendar/js/languages/bootstrap-year-calendar.eu.js',
-    './app/Resources/assets/js/bootstrap-year-calendar/js/languages/bootstrap-year-calendar.es.js',
-    './app/Resources/assets/js/bootstrap-table/dist/bootstrap-table.min.js',
-    './app/Resources/assets/js/bootstrap-table/dist/locale/bootstrap-table-eu-EU.js',
-    './app/Resources/assets/js/bootstrap-table/dist/extensions/export/bootstrap-table-export.min.js',
-    './app/Resources/assets/js/bootstrap-table/dist/extensions/export/tableExport.js',
-    './app/Resources/assets/js/bootstrap-table/dist/extensions/export/jspdf.js',
-    './app/Resources/assets/js/bootstrap-table/dist/extensions/export/jspdf.plugin.autotable.js',
-    './app/Resources/assets/js/libs/bootstrap-datepicker.min.js',
-    './app/Resources/assets/js/libs/bootstrap-popover.js',
-    './app/Resources/assets/js/bootbox/bootbox.min.js',
-    './app/Resources/assets/js/moment/min/moment-with-locales.min.js',
-    './app/Resources/assets/js/libs/bootstrap-datepicker.es.min.js',
-    './app/Resources/assets/js/libs/bootstrap-datepicker.eu.min.js',
-    './app/Resources/assets/js/app.js'
-];
-otherCSS = [
-    './app/Resources/assets/js/bootstrap/dist/css/bootstrap.css',
-    './app/Resources/assets/js/bootstrap/dist/css/bootstrap-theme.css',
-    './app/Resources/assets/js/bootstrap-year-calendar/css/bootstrap-year-calendar.min.css',
-    './app/Resources/assets/js/bootstrap-table/dist/bootstrap-table.css',
-    './app/Resources/assets/js/vertical-timeline/vertical-timeline.css',
-    './app/Resources/assets/js/libs/bootstrap-datepicker.min.css',
-    './app/Resources/assets/js/libs/select2.min.css',
-    './app/Resources/assets/'
+myIMG = [
+
 ];
 
-function onError(err) {
-    console.log(err);
-    this.emit('end');
-}
-
-// UTILS
-gulp.task('watch', function () {
-    gulp.watch('./app/Resources/assets/scss/**/*.scss', ['sass:dev']);
-    gulp.watch('./app/Resources/assets/js/*.js', ['js:dev'])
-});
-
-gulp.task('bower', function () {
-    return bower()
-        .pipe(gulp.dest(config.bowerDir))
-});
 
 gulp.task('clean', function () {
-    return del([
-        'web/css/*',
-        'web/js/*',
-        'web/fonts/*'
-    ]);
+    return del(['web/css/*','web/js/*','web/fonts/*']);
 });
 
 gulp.task('icons', function () {
-    return gulp.src(otherFonts)
-        .pipe(gulp.dest('./web/fonts'));
+    return gulp.src(myFonts).pipe(gulp.dest('./web/fonts'));
 
-});
-
-gulp.task('scss-lint', function () {
-    gulp.src([
-        paths.sass + '/**/*.scss',
-        '!' + paths.sass + '/base/_reset.scss'
-    ])
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(scsslint());
-});
-
-// CSS
-gulp.task('css:dev', function () {
-    return gulp.src(otherCSS)
-        .pipe(gulp.dest('web/css/'));
-});
-
-gulp.task('sass:dev', ['css:dev', 'scss-lint'], function () {
-    gulp.src(freeCSS)
-        .pipe(gulp.dest('web/css/'));
-
-    gulp.src(freeIMG)
-        .pipe(gulp.dest('web/img/'));
-    gulp.src(paths.sass)
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(sass({
-            errLogToConsole: true
-        }))
-        .pipe(postcss([cssnext]))
-        .pipe(gulp.dest(paths.buildCss))
-        .pipe(livereload());
-});
-
-gulp.task('sass:prod', function () {
-    gulp.src(freeCSS)
-        .pipe(gulp.dest('web/css/'));
-
-    gulp.src(freeIMG)
-        .pipe(gulp.dest('web/img/'));
-
-
-    return merge (
-        gulp.src(otherCSS)
-            .pipe(cssnano({keepSpecialComments: 1,rebase: false}))
-            .pipe(gulp.dest(paths.buildCss)),
-        gulp.src(paths.sass)
-            .pipe(plumber({errorHandler: onError}))
-            .pipe(sass({errLogToConsole: true}))
-            .pipe(postcss([cssnext]))
-            .pipe(cssnano({keepSpecialComments: 1,rebase: false}))
-            .pipe(gulp.dest(paths.buildCss))
-
-
-    ).pipe(concat('app.min.css')).pipe(gulp.dest(paths.buildCss));;
-
-
-    // return merge(niresass, besteCss).pipe(concat('app.min.css')).pipe(gulp.dest(paths.buildCss));
 });
 
 // JS
 gulp.task('js:dev', function () {
-    gulp.src(freeJS)
-      .pipe(babel({presets: ['es2015']}))
-      .pipe(gulp.dest('web/js/'));
-    return gulp.src(otherJS)
+    return gulp.src(myJS)
+        .pipe(babel({presets: ['es2015']}))
         .pipe(gulp.dest('web/js/'));
 });
 
 gulp.task('js:prod', function () {
-    gulp.src(freeJS)
-      .pipe(babel({presets: ['es2015']}))
-      .pipe(minify())
-      .pipe(gulp.dest('web/js/'));
-    return gulp.src(otherJS)
-        .pipe(minify())
-        .pipe(concat('app.min.js'))
-        .pipe(gulp.dest('web/js/'));
+    return gulp.src(myJS)
+               .pipe(babel({presets: ['es2015']}))
+               .pipe(minify())
+               .pipe(concat('app.min.js'))
+               .pipe(gulp.dest('web/js/'));
 });
 
 
-// Task guztiak batuz
-gulp.task('prod', ['icons', 'js:prod', 'sass:prod']);
+gulp.task('css:dev', function () {
+    gulp.src(myIMG).pipe(gulp.dest('web/img/'));
+    gulp.src(myCSS).pipe(gulp.dest('web/css/'));
+});
 
-gulp.task('dev', ['icons', 'js:dev', 'sass:dev']);
+gulp.task('css:prod', function () {
+    gulp.src(myIMG).pipe(gulp.dest('web/img/'));
+    return merge (
+        gulp.src(myCSS)
+            .pipe(cssnano({keepSpecialComments: 1,rebase: false}))
 
-gulp.task('default', ['clean', 'icons', 'js:dev', 'sass:dev', 'watch']);
+        ).pipe(concat('app.min.css')).pipe(gulp.dest(paths.buildCss));
+});
 
-// gulp.task('prod', ['sass:prod', 'modernizr', 'js:prod']);
+gulp.task('watch', function () {
+    gulp.watch(myCSS, ['css:dev']);
+    gulp.watch(myJS, ['js:dev'])
+});
+
+gulp.task('dev', ['icons', 'js:dev', 'css:dev']);
+
+gulp.task('prod', ['clean','icons', 'js:prod', 'css:prod']);
+
+gulp.task('default', ['clean', 'icons', 'js:dev', 'css:dev', 'watch']);
